7574251471d7a35da98e32aaa15a82a87fdb97ac
Ozzymand <24418935+Ozzymand@users.noreply.github.com>
2021-12-07 17:03:52 +0200

    Fixed fatal crash

--- Calculator/main.js
+++ Calculator/main.js
@@ -7,6 +7,53 @@ var values = {
     result: null,
     history: null
 }
+var showRes = false;
+
+var wait = (ms) => {
+    const start = Date.now();
+    let now = start;
+    while (now - start < ms) {
+      now = Date.now();
+    }
+}
+
+function fullClear(){
+    // CLEARS ALL REGISTRY
+    values.num1 = [];
+    values.num2 = [];
+    values.numInt1 = '';
+    values.numInt2 = '';
+    values.operator = null;
+    values.result = null;
+    showRes = false;
+}
+
+function softClear(){
+    values.num2 = [];
+    values.numInt2 = '';
+    values.result = null;
+    values.operator = null;
+    showRes = false;
+}
+
+function calculate(){
+    values.numInt1 = Number(values.numInt1);
+    values.numInt2 = Number(values.numInt2);
+    if(values.operator == "+"){
+        values.result = values.numInt1 + values.numInt2;
+        values.history = `${values.numInt1} + ${values.numInt2}`;
+    } else if(values.operator == "-"){
+        values.result = values.numInt1 - values.numInt2;
+        values.history = `${values.numInt1} - ${values.numInt2}`;
+    } else if(values.operator == "*"){
+        values.result = values.numInt1 * values.numInt2;
+        values.history = `${values.numInt1} * ${values.numInt2}`;
+    } else if(values.operator == "/"){
+        values.result = values.numInt1 / values.numInt2;
+        values.history = `${values.numInt1} / ${values.numInt2}`;
+    }
+    if(showRes == true){document.getElementById('result').innerHTML = values.result;}
+}
 
 addEventListener('click', function(onClick){
         if(values.num1[0] == null){
@@ -18,60 +65,50 @@ addEventListener('click', function(onClick){
             inputItem = onClick.target.innerHTML;
             if(inputItem <= 9){
                 // make number switch when operator is present
-                if(values.num1[0] != null && values.operator == null){
-                    values.num1.push(Number(inputItem))
+                if(values.operator == null){
+                    values.num1.push(Number(inputItem));
                 }                
                 if(values.operator != null){
-                    values.num2.push(Number(inputItem))
+                    values.num2.push(Number(inputItem));
                 }
             };
             if(inputItem == "+" || inputItem == "-" || inputItem == "/" || inputItem == "*"){
                 values.operator = inputItem;
-            } else if(inputItem == "C"){
-                // CLEARS ALL REGISTRY
-                values.num1 = [];
-                values.num2 = [];
-                values.numInt1 = '';
-                values.numInt2 = '';
-                values.operator = null;
-            } else if(inputItem == "="){
-                // r/theyDidTheMath
-                values.numInt1 = Number(values.numInt1)
-                values.numInt2 = Number(values.numInt2)
-                if(values.operator == "+"){
-                    values.result = values.numInt1 + values.numInt2;
-                    values.history = `${values.numInt1} + ${values.numInt2}`;
-                } else if(values.operator == "-"){
-                    values.result = values.numInt1 - values.numInt2;
-                    values.history = `${values.numInt1} - ${values.numInt2}`;
-                } else if(values.operator == "*"){
-                    values.result = values.numInt1 * values.numInt2;
-                    values.history = `${values.numInt1} * ${values.numInt2}`;
-                } else if(values.operator == "/"){
-                    values.result = values.numInt1 / values.numInt2;
-                    values.history = `${values.numInt1} / ${values.numInt2}`;
-                }
-                document.getElementById('result').innerHTML = values.result;
             }
-                
-
+            if(inputItem == "CE"){
+                fullClear()
+            }
+            if(inputItem == "="){
+                // r/theyDidTheMath
+                showRes = true;
+                calculate();
+            }
     }
 });
 
 addEventListener('click', function(e){
-    console.log(values)
+    // Write out number 1
     if(values.num1 != null && values.operator == null){
-        values.numInt1 = [];
+        values.numInt1 = '';
         for(var i = 0; i < values.num1.length; i++){
             values.numInt1 = values.numInt1 + values.num1[i]
         }
         document.getElementById('result').innerHTML = values.numInt1;
-    } else if(values.num1 != null && values.operator != null && values.num2 != null && values.result == null) {
-        values.numInt2 = [];
+    } 
+    // Write out number 2 
+    else if(values.operator != null && showRes == false) {
+        values.numInt2 = '';
         for(var i = 0; i < values.num2.length; i++){
             values.numInt2 = values.numInt2 + values.num2[i]
         }
         document.getElementById('result').innerHTML = values.numInt1 + values.operator + values.numInt2;  
     } 
+    // Write out of result exists
+    else if(values.result != null && values.operator != null) {
+        values.numInt1 = Number(values.result);
+        softClear();
+    }
     document.getElementById('history').innerHTML = values.history;
+    console.log(values)
+    console.log(showRes)
 });
